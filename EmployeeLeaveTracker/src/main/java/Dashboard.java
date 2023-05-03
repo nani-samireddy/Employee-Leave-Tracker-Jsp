a6d1adeef5a6553d1d848054f76b3be54e265e34
nani samireddy <60937910+nani-samireddy@users.noreply.github.com>
2023-05-03 23:57:38 +0530

    updated comment in Dashboard.java

--- EmployeeLeaveTracker/src/main/java/Dashboard.java
+++ EmployeeLeaveTracker/src/main/java/Dashboard.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
+/** updated here
  * Servlet implementation class Dashboard
  */
 @WebServlet("/Dashboard")
